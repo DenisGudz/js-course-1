5b7cce7e797173705c89af1ce49e4e7904664103
Taras Gudz <taras.mail.main@gmail.com>
2023-11-09 08:00:35 +0200

    all operations working

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,66 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-function add() {
-    const currentResult = currentResult + userInput.value;
-    outputReslut("currentResult", '');
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription)
+} 
+
+function writeToLog(
+    operatoinIdentifier, 
+    prevResult, 
+    operationNumber, 
+    newResult
+    ) {
+    const logEntry = {
+        operation: operatoinIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry)
+    console.log(logEntries)
+
 }
 
-addBtn.addEventListner('click', add)
+function add() {
+    const enteredNumber = +userInput.value
+    const initialResult = currentResult;
+    const calcDescription = `${currentResult} + ${enteredNumber}`
+    currentResult = currentResult + +enteredNumber;
+    outputResult(currentResult, calcDescription);
+    writeToLog('add', initialResult, enteredNumber, currentResult)
+
+}
+
+function subtract() {
+    const enteredNumber = +userInput.value;
+    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult, calcDescription);
+
+   
+}
+
+function multiply() {
+    const enteredNumber = +userInput.value
+    const calcDescription = `${currentResult} * ${enteredNumber}`
+    currentResult = currentResult * enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+    const enteredNumber = +userInput.value
+    const calcDescription = `${currentResult} / ${enteredNumber}`
+    currentResult = currentResult / enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
